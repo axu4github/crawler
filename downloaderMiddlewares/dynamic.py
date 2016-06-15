4205083a247802cd9d0ebf1666bdbe9f30aa3d3d
axu4github <axu@axu4iMac.local>
2016-06-15 20:56:48 +0800

    完成使用中间件，解决动态页面解析

--- downloaderMiddlewares/dynamic.py
+++ downloaderMiddlewares/dynamic.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*- 
 from selenium import webdriver
 import time
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
-# from scrapy.http import Response
-
-# class DynamicResposne(Response):
-#     def __init__(self):
-#         Response.__init__(self)
 
 class DynamicPageMiddleware(object):
     
     def __init__(self):
-        self.driver = webdriver.PhantomJS()
+        self.driver = webdriver.Chrome()
 
     def __del__(self):
         self.driver.close()
 
     def process_response(self, request, response, spider):
-        self.driver.get(request.url)
-        return response.replace(body=self.driver.page_source)
+        url = request.url
+        # 若不是抓取连接则直接返回response
+        if url not in spider.start_urls:
+            return response
+        else:   
+            self.driver.get(request.url)
+            time.sleep(10)
+            return response.replace(body=self.driver.page_source.encode('utf-8'))
+
+    def process_exception(self, request, exception, spider):
+        self.driver.close()
+        
